9bd1826febc11a363b806f63f4cfce2336c9ed3c
gasilyas <il.gasin@yandex.ru>
2026-09-06 08:40:48 +0300

    fix BurgerConstructorTest

--- src/test/java/ru/yandex/practicum/stellarburgers/ui/BurgerConstructorTest.java
+++ src/test/java/ru/yandex/practicum/stellarburgers/ui/BurgerConstructorTest.java
@@ -1,24 +1,41 @@
 package ru.yandex.practicum.stellarburgers.ui;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import ru.yandex.practicum.stellarburgers.ui.pages.MainPage;
 
 public class BurgerConstructorTest extends BasicSeleniumTestConfiguration {
 
+    private MainPage mainPage;
+
+    @BeforeEach
+    public void initPages() {
+        mainPage = new MainPage(driver);
+    }
+
     @Test
-    @DisplayName("Проверка переключения вкладок в конструкторе бургеров: Булки, Соусы, Начинки")
-    public void testBurgerAssemblerTabsSwitching() {
-        MainPage mainPage = new MainPage(driver);
-
+    @DisplayName("Проверка переключения вкладок. Вкладка Булки в конструкторе бургеров")
+    public void testBurgerAssemblerTabBunsSwitch() {
         mainPage.clickSaucesTab();
-        Assertions.assertTrue(mainPage.checkSaucesTabActive(), "Вкладка 'Соусы' не стала активной");
-
-        mainPage.clickFillingsTab();
-        Assertions.assertTrue(mainPage.checkFillingsTabActive(), "Вкладка 'Начинки' не стала активной");
-
         mainPage.clickBunsTab();
         Assertions.assertTrue(mainPage.checkBunsTabActive(), "Вкладка 'Булки' не стала активной после переключения");
+    }
+
+    @Test
+    @DisplayName("Проверка переключения вкладок. Вкладка Соусы в конструкторе бургеров")
+    public void testBurgerAssemblerTabSaucesSwitch() {
+
+        mainPage.clickSaucesTab();
+        Assertions.assertTrue(mainPage.checkSaucesTabActive(), "Вкладка 'Соусы' не стала активной после переключения");
+    }
+
+    @Test
+    @DisplayName("Проверка переключения вкладок. Вкладка Начинки в конструкторе бургеров")
+    public void testBurgerAssemblerTabFillingsSwitch() {
+
+        mainPage.clickFillingsTab();
+        Assertions.assertTrue(mainPage.checkFillingsTabActive(), "Вкладка 'Начинки' не стала активной после переключения");
     }
 }
